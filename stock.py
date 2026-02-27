e670ead196d91ab87b50359721f2502981279a88
alexkelly <kelly.j.alex@gmail.com>
2026-02-27 12:53:49 -0800

    Added new stock class, renamed main file.

---
+++ stock.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+
+import finnhub
+import pandas as pd
+from math import sqrt
+
+
+class stock:
+    def __init__(self,
+                 symbol: str = None,
+                 nextEarnings:datetime = None,
+                 ratings: pd.DataFrame = None,
+                 confidence: float = None,
+                 congruency: float = None,
+                 lastUpdate: datetime = None,
+                 APIclient: finnhub.APIClient = None):
+
+        self.symbol = symbol
+        self.nextEarnings = nextEarnings
+        self.ratings = ratings
+        self.confidence = confidence
+        self.congruency = congruency
+        self.lastUpdate = lastUpdate
+        self.APIclient = APIclient
+
+
+        if self.symbol is not None and self.APIclient is not None:
+            self.updateInfo()
+
+
+    def updateInfo(self):
+        self.ratings = self.APIclient.recommendation_trends(self.symbol)
+        ratings = pd.DataFrame(self.ratings)
+
+
+        if self.ratings.empty:
+            data = {'Confidence': 0,
+                    'Period': 0,
+                    'Congruency': 0,
+                    'Symbol': self.symbol}
+            return (data)
+
+        ratings = self.ratings.sort_values(by='period', ascending=False)  # sort by most recent
+        latest = ratings.iloc[0]  # isolate most recent ratings
+        # print(ratings)
+
+        period = latest['period'] #last updated
+        weighted = {
+            -2: -2 * latest['strongSell'],
+            -1: -1 * latest['sell'],
+            0: 0 * latest['hold'],
+            1: latest['buy'],
+            2: 2 * latest['strongBuy']  # still not great, need to improve
+        }
+
+        N = sum(weighted.values())  # this is wrong, N should be sum of values * their weights
+        if N == 0:
+            raise Exception('No data')
+
+        mean = sum(score * count for score, count in weighted.items()) / N
+        variance = sum(count * (score - mean) ** 2
+                       for score, count in weighted.items()) / N
+
+        std_dev = sqrt(variance)
+
+        confidence = float(round(mean, 2))  # wrong, need to figure how tf this returns values of >100%
+        congruency = round((1 - (std_dev / 2)), 2)
+
+        data = {'Confidence': confidence,
+                'Period': period,  # how up to date ratings are
+                'Congruency': congruency,
+                'Symbol': self.symbol}
+
+        self.confidence = confidence
+        self.congruency = congruency
+        self.lastUpdate = period
+
+        return (data)
